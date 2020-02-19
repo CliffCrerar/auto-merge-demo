1b695478a28b6cc1e41f58ac0f9dbe3352e118a6
Cliff Crerar <cliff@gmai.com>
2020-02-20 01:09:26 +0200

    continuing demo

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -8,36 +8,53 @@ let val = ''
 
 export default html`
 	<style>
-		dialog{
+		dialog {
+			width: 500px;
 			top: 30%;
 			left: 30%;
 		}
 	</style>
-	<div class="dialog text-center">
+	<div class="dialog text-center w-100">
 		<label for="#cardnameinput">Enter Name for Card</label>	
-		<input type="text" id="cardnameinput" name="card-name-input" class="input" .value=${val}>
-			<button @click=${onOkBtnClick} type="button" class="w-100">Ok</button>
-			<button @click=${onCancelBtnClick} type="reset" class="w-100">cancel</button>
+		<input 
+			type="text" 
+			id="cardnameinput" 
+			name="card-name-input" 
+			class="input" 
+			.value=${val }>
+		<label 
+			for="#carddescinput">Card Description</label>
+		<textarea 
+			id="carddescinput" 
+			name="description">
+		</textarea>
+			<button 
+				@click=${onOkBtnClick } 
+				type="button" 
+				class="w-100">Ok</button>
+			<button 
+				@click=${onCancelBtnClick } 
+				type="reset" 
+				class="w-100">cancel</button>
 	</div>
 `
 
-val = function(v){
-	console.log(v);
+val = function (v) {
+	let value
+	console.log('v', v);
 }
 
-
-function onOkBtnClick(){
+function onOkBtnClick() {
 	console.log('Click ok');
 	const inputValue = getValue();
 }
 
-function onCancelBtnClick(){
+function onCancelBtnClick() {
 	console.log('Click Cancel');
-	
 }
 
-function getValue(){return byName('card-name-input')[0].value;}
+function getValue() { return byName('card-name-input')[ 0 ].value; }
 
 function inputValid() {
-	return getValue()!==''
+	return getValue() !== '';
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 /**
  * Component index
  */
-import {render,html} from 'lit-html';
+import { render, html } from 'lit-html';
 import header from './header/header'
 import swimLanes from './main/swim-lanes';
 import footer from './footer/footer';
@@ -11,9 +11,8 @@ const sw = swimLanes();
 const content = () => html`
 <div 
 	id="sw-container"
-	class="d-flex border-right" style="flex: 1;" 
-	>
-	${sw}
+	class="d-flex border-right" style="flex: 1;" >
+	${sw }
 </div>
 `
-export {content,swimLanes,header,footer} 
+export { content, swimLanes, header, footer } 
